c606f57f8010c545530199d842947853adb7271c
fuzusheng <fuzusheng@gmail.com>
2021-01-18 11:48:52 +0800

    lr

--- src/main/java/com/fuzs/linearregression/App.java
+++ src/main/java/com/fuzs/linearregression/App.java
@@ -10,6 +10,7 @@ import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.IterativeDataSet;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 
 public class App {
@@ -17,7 +18,8 @@ public class App {
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         DataSet<Tuple2<Double, Double>> lr = env.fromElements(new Tuple2<Double, Double>(1.0, 0.0));
         DataSet<Tuple2<Double, Double>> data = KMeansData.getDefaultPointDataSet(env);
-        IterativeDataSet<Tuple2<Double, Double>> lrIte = lr.iterate(4);
+        
+        IterativeDataSet<Tuple2<Double, Double>> lrIte = lr.iterate(400);
 
         DataSet<Tuple2<Double, Double>>  iterBody =  data.map(new calDiff()).withBroadcastSet(lrIte, "lr")
         .reduce(new calSum())
@@ -29,7 +31,7 @@ public class App {
 
     }
 
-    public static final class calDiff extends RichMapFunction<Tuple2<Double, Double>, Tuple2<Double, Double>> {
+    public static final class calDiff extends RichMapFunction<Tuple2<Double, Double>, Tuple3<Double, Double,Long>> {
 
         private List<Tuple2<Double, Double>> lr;
 
@@ -39,23 +41,23 @@ public class App {
         }
 
         @Override
-        public Tuple2<Double, Double> map(Tuple2<Double, Double> v) throws Exception {
-            return new Tuple2(lr.get(0).f0 * v.f0 - v.f1,(lr.get(0).f0 * v.f0 - v.f1)*v.f0);
+        public  Tuple3<Double, Double,Long> map(Tuple2<Double, Double> v) throws Exception {
+            return new Tuple3(lr.get(0).f0 * v.f0 - v.f1,(lr.get(0).f0 * v.f0 - v.f1)*v.f0,1L);
         }
     }
 
-    public static final class calSum implements ReduceFunction<Tuple2<Double, Double>>{
+    public static final class calSum implements ReduceFunction<Tuple3<Double, Double,Long>>{
 
         @Override
-        public Tuple2<Double, Double> reduce(Tuple2<Double, Double> arg0, Tuple2<Double, Double> arg1)
+        public Tuple3<Double, Double,Long> reduce(Tuple3<Double, Double,Long> arg0, Tuple3<Double, Double,Long> arg1)
                 throws Exception {
 
-            return new Tuple2(arg0.f0 + arg1.f0,arg0.f1 + arg1.f1);
+            return new Tuple3(arg0.f0 + arg1.f0,arg0.f1 + arg1.f1,arg0.f2+arg1.f2);
         }
 
     }
 
-    public static final class calRes extends RichMapFunction<Tuple2<Double, Double>, Tuple2<Double, Double>> {
+    public static final class calRes extends RichMapFunction<Tuple3<Double, Double,Long>, Tuple2<Double, Double>> {
 
         private List<Tuple2<Double, Double>> lr;
 
@@ -65,8 +67,8 @@ public class App {
         }
 
         @Override
-        public Tuple2<Double, Double> map(Tuple2<Double, Double> v) throws Exception {
-            return new Tuple2(lr.get(0).f0 - v.f0 * (1/80),lr.get(0).f1 - v.f1 * (1/80));
+        public Tuple2<Double, Double> map(Tuple3<Double, Double,Long> v) throws Exception {
+            return new Tuple2(lr.get(0).f0 - v.f0 * (0.5/v.f2),lr.get(0).f1 - v.f1 * (0.5/v.f2));
         }
     }
 
